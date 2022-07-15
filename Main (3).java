680b3f7b8554acd56bdd4018257eea1b5f5b6595
Writecodes <108628113+Writecodes@users.noreply.github.com>
2022-07-15 17:00:25 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,41 @@
+//..................Find the sum of two matrices.....................................
+public class Main
+{
+	public static void main(String[] args) {
+// 	int i,j;
+//         int[][] A={ {2, 3, 4}, {5, 2, 3} };
+//         int[][] B={ {-4, 5, 3}, {5, 6, 3} };
+//         int C[][]={{0,0,0},{0,0,0}};
+//         System.out.println("the sum of two given matrices : ");
+//         for(i=0;i< A.length;i++){
+//             for(j=0;j<B[i].length;j++){
+//                 C[i][j]=A[i][j]+B[i][j];
+//                 System.out.print(C[i][j]+" ");
+//             }
+//             System.out.println("");
+//         }
+
+
+//..............Multiplication of matrices without using methods and user input................
+//  int[][] A = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+//         int[][] B = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+//         int[][] C = new int[3][3];
+//         int i, j, k;
+//         System.out.println("The multiplication of two given matrices : ");
+//         for (i=0;i<3;i++) {
+//             for (j=0;j<3;j++) {
+//                 C[i][j]=0;
+//                 for (k=0;k<3;k++) {
+//                     C[i][j]+=A[i][k]*B[k][j];
+//                 }
+//                 System.out.print(C[i][j]+" ");
+//             }
+//             System.out.println("");
+//         }
+
+
+//..........Multiplication of matrices with using methods and user input................
+
+    }
+}
+	
